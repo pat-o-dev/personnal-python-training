a7cd1036101362b40253d448ddb513cf4224cb64
Patpatrouille <patrickgenitrini@gmail.com>
2025-07-24 14:14:01 +0700

    just HellovWorld first

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # personnal-python-training
 Believe in the future, just do it
+
+# script in python without help from IA
+
+> python -m venv .venv #envirronement virtuel pour eviter de crash le systeme
---
+++ caculator/main.py
@@ -0,0 +1,12 @@
+# on cree une calculatrice basique avec tkinter
+
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+frm = ttk.Frame(root, padding=25)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0,row=1)
+root.mainloop()
