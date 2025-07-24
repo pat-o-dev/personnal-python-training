7efd5622807115d4da72299a5420faed8f28c2f2
Patpatrouille <patrickgenitrini@gmail.com>
2025-07-24 14:33:54 +0700

    gestion d une variable avec champ text avec tk

--- caculator/main.py
+++ caculator/main.py
@@ -1,12 +1,27 @@
 # on cree une calculatrice basique avec tkinter
+# hyper basique, 4 columns 5 rows
+import tkinter as tk
 
-from tkinter import *
-from tkinter import ttk
-
-root = Tk()
-
-frm = ttk.Frame(root, padding=25)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0,row=1)
-root.mainloop()
+class App(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        
+        self.pack()
+        
+        self.entrythingy = tk.Entry()
+        self.entrythingy.pack()
+        
+        self.contents = tk.StringVar()
+        self.contents.set("contenu de la variable")
+        self.entrythingy["textvariable"] = self.contents
+        
+        self.entrythingy.bind('<Key-Return>',
+                              self.print_contents)
+        
+    def print_contents(self, event):
+        print("Contenu actuel :",
+              self.contents.get())
+        
+root = tk.Tk()
+app = App(root)
+app.mainloop()
